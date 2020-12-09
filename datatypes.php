ef00d466beff7865feedd73c71e0ba1170348669
Manuel Kessel <liproqq@gmail.com>
2020-12-09 10:47:26 +0100

    datatypes

---
+++ datatypes.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $string = "to be or not to be";
+        $int = 5;
+        $float = 30.2;
+        $bool = TRUE;
+    ?>
+</body>
+</html>
---
+++ site.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+        <?php
+        $name = "Peter";
+        $age = 35;
+        echo "My name is $name <br>";
+        echo "I'm $age years old<br>";
+        $name = "Mike";
+        echo "I like my name $name<br>";
+        echo "I like being $age years old<br>";
+        ?>
+
+    </body>
+</html>
