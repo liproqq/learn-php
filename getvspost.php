57043e446040285971d2a10c09fb3c1b4dd3538a
Manuel Kessel <liproqq@gmail.com>
2020-12-09 11:33:45 +0100

    get vs post

---
+++ getvspost.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="getvspost.php" method="post">
+        Password: <input type="password" name="password">
+        <input type="submit">
+    </form>
+
+    <?php
+        echo $_POST["password"], "<br>";
+    ?>
+
+</body>
+</html>
