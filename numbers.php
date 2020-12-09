78c014abc7937159a80e3e0f10c03180659f1dc6
Manuel Kessel <liproqq@gmail.com>
2020-12-09 11:03:47 +0100

    numbers

---
+++ numbers.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $num = 55;
+        $num++;
+        echo $num, "<br>";
+        $num/=-2;
+        echo $num, "<br>";
+        echo pow($num, 3), "<br>";
+        echo max($num, 5);
+    ?>
+</body>
+</html>
