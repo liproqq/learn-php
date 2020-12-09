4abe8ee53ea28d822b6df2681e10d34e334cb732
Manuel Kessel <liproqq@gmail.com>
2020-12-09 11:11:50 +0100

    input

---
+++ input.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="input.php" method="get">
+        Name: <input type="text" name="name">
+        <input type="submit">
+    </form>
+
+    your name is:
+
+    <?php
+        echo $_GET["name"];
+    ?>
+
+</body>
+</html>
