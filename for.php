b522558a941e035580ca452059856f890bf91c2d
Manuel Kessel <liproqq@gmail.com>
2020-12-09 13:45:51 +0100

    for loop

---
+++ for.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        $fibo = array(0, 1, 1, 2, 3, 5, 8, 13);
+
+        for ($i=0; $i < count($fibo); $i++) { 
+            echo $fibo[$i], "<br>";
+        }
+        /*
+        // comment
+        */
+    ?>
+
+</body>
+</html>
